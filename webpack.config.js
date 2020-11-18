893b1e3c0d69872fe883d9d757ea2084a36e8521
Nii Yeboah <nii@vaadin.com>
2020-11-18 14:23:20 +0200

    Update demo

--- webpack.config.js
+++ webpack.config.js
@@ -113,12 +113,7 @@ const productionConfig = merge([
     plugins: [
       new CopyWebpackPlugin([...polyfills, ...assets]),
       new HtmlWebpackPlugin({ template: INDEX_TEMPLATE }),
-      new HtmlReplaceWebpackPlugin([
-        {
-          pattern: /<script dev.*?src=".*?\.js".*?<\/script>/g,
-          replacement: ''
-        }
-      ]),
+      new HtmlReplaceWebpackPlugin([{ pattern: /<script dev.*?src=".*?\.js".*?<\/script>/g, replacement: '' }]),
       new HtmlWebpackTagsPlugin({ tags: ['vendor/webcomponents-bundle.js'], append: false }),
       new CompressionPlugin({ test: /\.js(\.map)?$/i }),
       new BrotliPlugin({
