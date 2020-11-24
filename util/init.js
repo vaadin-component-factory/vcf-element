b05e2dbf3ac641b3e7345859686dc69fbd4f404b
Nii Yeboah <nii@vaadin.com>
2020-11-24 20:48:30 +0200

    Fix util scripts

--- util/init.js
+++ util/init.js
@@ -1,2 +1,18 @@
 /* THIS SCRIPT IS RUN AUTOMATICALLY AFTER NPM INSTALL. */
-require('@vaadin-component-factory/vcf-element-util').initialize(__dirname);
+const { initialize } = require('@vaadin-component-factory/vcf-element-util');
+
+initialize(
+  __dirname,
+  [
+    'index.html',
+    'README.md',
+    'package.json',
+    'vcf-element.ts',
+    'demo/demo.js',
+    'demo/index.html',
+    'src/vcf-element.ts',
+    'test/vcf-element.test.ts',
+    'util/publish.js'
+  ],
+  ['vcf-element.ts', 'src/vcf-element.ts', 'test/vcf-element.test.ts']
+);
